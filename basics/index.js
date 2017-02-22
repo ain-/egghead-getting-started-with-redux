141ef5fd8a0d01e320a28ca984e817a132f517db
Ain <ain.salula@gmail.com>
2017-02-22 05:19:37 +0200

    basic dispatch example

---
+++ basics/index.js
@@ -0,0 +1,18 @@
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const { createStore } = Redux;
+const store = createStore(counter);
+
+console.log(store.getState());
+
+store.dispatch({ type: 'INCREMENT'});
+console.log(store.getState());
