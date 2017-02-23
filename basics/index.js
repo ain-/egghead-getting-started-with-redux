c69befbcc84e9c671bebbd19e21c490438c57377
Ain <ain.salula@gmail.com>
2017-02-23 02:21:56 +0200

    implementing store from scratch

--- basics/index.js
+++ basics/index.js
@@ -9,7 +9,30 @@ const counter = (state = 0, action) => {
   }
 }
 
-const { createStore } = Redux;
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listener = listeners.filter(l => l !== listener);
+    };
+  }
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
+
+//const { createStore } = Redux;
 const store = createStore(counter);
 
 const render = () => {
