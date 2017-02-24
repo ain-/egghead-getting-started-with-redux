ec67ab1f376f3a39f32ac3650c8cb8f871c9f876
Ain <ain.salula@gmail.com>
2017-02-24 15:24:50 +0200

    add todo from input

--- src/todo.js
+++ src/todo.js
@@ -12,12 +12,16 @@ class TodoApp extends React.Component {
   render() {
     return (
       <div>
+        <input ref={node => {
+          this.input = node;
+        }} />
         <button onClick={() => {
           store.dispatch({
             type: 'ADD_TODO',
-            text: 'Test',
+            text: this.input.value,
             id: nextTodoId++
           });
+          this.input.value = '';
         }}>
           Add todo
         </button>
