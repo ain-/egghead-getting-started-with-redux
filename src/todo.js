0c7da24cc9f575420c9b93d853952601f82839fb
Ain <ain.salula@gmail.com>
2017-02-26 18:04:12 +0200

    extract container components (FilterLink)

--- src/todo.js
+++ src/todo.js
@@ -7,20 +7,19 @@ const todoApp = combineReducers({todos, visibilityFilter});
 
 const store = createStore(todoApp);
 
-const FilterLink = ({
-  filter,
-  currentFilter,
+const Link = ({
+  active,
   children,
   onClick
 }) => {
-  if (filter === currentFilter) {
+  if (active) {
     return <span>{children}</span>;
   }
   return (
     <a href='#'
       onClick={e => {
         e.preventDefault();
-        onClick(filter);
+        onClick();
       }}
     >
       {children}
@@ -28,26 +27,50 @@ const FilterLink = ({
   );
 };
 
-const Footer = ({
-  visibilityFilter,
-  onFilterClick
-}) => (
+class FilterLink extends React.Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const props = this.props;
+    const state = store.getState();
+
+    return (
+      <Link
+        active={
+          props.filter === state.visibilityFilter
+        }
+        onClick={() =>
+          store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter: props.filter
+          })
+        }
+      >
+        {props.children}
+      </Link>
+    );
+  }
+}
+
+const Footer = () => (
   <p>
     Show:
     {' '}
     <FilterLink filter='SHOW_ALL'
-      currentFilter={visibilityFilter}
-      onClick={onFilterClick}
     >All</FilterLink>
     {' '}
     <FilterLink filter='SHOW_ACTIVE'
-      currentFilter={visibilityFilter}
-      onClick={onFilterClick}
     >Active</FilterLink>
     {' '}
     <FilterLink filter='SHOW_COMPLETED'
-      currentFilter={visibilityFilter}
-      onClick={onFilterClick}
     >Completed</FilterLink>
   </p>
 );
@@ -152,7 +175,7 @@ const TodoApp = ({
             filter
           })
         }
-        />    
+        />
     </div>
 );
 
